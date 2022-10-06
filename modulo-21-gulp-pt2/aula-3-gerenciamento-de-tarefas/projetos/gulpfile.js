0b4638fd8955ff4edf8481b442a36add112cd393
caduzeira <carlosefaraujo333@gmail.com>
2022-10-05 21:44:27 -0300

    gulp-pt2

---
+++ modulo-21-gulp-pt2/aula-3-gerenciamento-de-tarefas/projetos/gulpfile.js
@@ -0,0 +1,51 @@
+const gulp = require('gulp')
+const concat = require('gulp-concat')
+const min = require('gulp-cssmin')
+const rename = require('gulp-rename')
+const ugly = require('gulp-uglify')
+const htmlmin = require('gulp-htmlmin');
+const { parallel } = require('gulp')
+const babel = require('gulp-babel')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
+
+function tarefaCss(cb){
+    return gulp.src('./src/**/*.css')
+        .pipe(concat('libs.css'))
+        .pipe(min())
+        .pipe(rename({suffix:'.min'}))
+        .pipe(gulp.dest('./dist/css'))
+}
+
+function tarefaJs(cb){
+    return gulp.src('./src/**/*.js')
+        .pipe(babel({
+            comments:false,
+            presets:['@babel/env']
+        }))
+        .pipe(concat('libs.js'))
+        .pipe(ugly())
+        .pipe(rename({suffix:'.min'}))
+        .pipe(gulp.dest('./dist/js'))
+}
+
+function tarefaHtml(cb){
+    return gulp.src('./src/**/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./dist'))
+}
+
+gulp.task('serve',function(){
+
+    browserSync.init({
+        server:{
+            baseDir: './dist'
+        }
+    })
+    gulp.watch('./src/**/*').on('change', process)
+    gulp.watch('./src/**/*').on('change', reload)
+})
+
+const process = parallel(tarefaCss, tarefaHtml, tarefaJs)
+
+exports.default = process
